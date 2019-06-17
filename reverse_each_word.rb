acb3c841b1d8e2c4e4ec502cc2c93d053309e74f
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 17:47:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -19,6 +19,5 @@ end
 #.collect
 def reverse_each_word(sentence)
   sentence_array = sentence.split(/ /) 
-  sentence_array.collect {|word| word.reverse}
-  final_sentence
+  final_sentence = sentence_array.collect {|word| word.reverse}
 end
