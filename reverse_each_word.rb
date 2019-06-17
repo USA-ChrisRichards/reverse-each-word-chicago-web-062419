9517ad805cb65b7e2dabcef2941635b8222a71a0
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 17:39:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 #returns that same sentence with each word reversed in place.
 #1.how can we turn our string into an array?
 #2.first solve with .each, then .collect
-def reverse_each_word(sentence) 
+def reverse_each_word(sentence)    #.each
   sentence_array = sentence.split(/ /) 
   final_sentence = ""
   rev_array = []
