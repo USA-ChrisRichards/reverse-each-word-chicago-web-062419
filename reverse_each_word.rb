2300f6d64d055b2219ff465b71918d93b51a5695
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 17:24:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,15 +2,10 @@
 #1.how can we turn our string into an array?
 #2.first solve with .each, then .collect
 def reverse_each_word(sentence) 
-    #give method a string/sentence
-  
   sentence_array = sentence.split(' ') 
-    #split string into array by each word
-  
   rev_array = []
-  sentence_array.each do |word| 
+    sentence_array.each do |word| 
     word.reverse << rev_array 
-    #enumerate with each to reverse each word, then add to new array(rev_array)
   end
   rev_array
 end
