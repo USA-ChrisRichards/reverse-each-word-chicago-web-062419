b0b1d99246b5bdb139a19dfca58878c22aba57a2
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 17:26:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 #1.how can we turn our string into an array?
 #2.first solve with .each, then .collect
 def reverse_each_word(sentence) 
-  sentence_array = sentence.split(' ') 
+  sentence_array = sentence.split(/ /) 
   rev_array = []
     sentence_array.each do |word| 
     word.reverse << rev_array 
