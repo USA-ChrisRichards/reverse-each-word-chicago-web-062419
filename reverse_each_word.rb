d1c436bd5d529c1e116e5b46ca5b448ea7f4aee4
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 17:17:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,6 +7,8 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(" ") 
     #split string into array by each word
   
-  sentence_array.each { |word| word.reverse }
+  rev_array = sentence_array.each { |word| word.reverse << rev_array }
     #enumerate with each to reverse each word
+    
+  
 end
