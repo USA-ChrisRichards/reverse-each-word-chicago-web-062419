3b315fd7ae1756f6b4260fd391e4c6ea664077ae
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 17:33:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@
 #2.first solve with .each, then .collect
 def reverse_each_word(sentence) 
   sentence_array = sentence.split(/ /) 
-  puts sentence_array
+  print sentence_array
   rev_array = []
     sentence_array.each do |word| 
     word.reverse << rev_array 
