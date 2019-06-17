680a32129ce6a701c39edfe3d0aa940c07d882b7
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-17 17:11:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@
 #1.how can we turn our string into an array?
 #2.first solve with .each, then .collect
 def reverse_each_word(sentence) 
-  sentence.split(" ")
-  puts sentence
+  sentence_array = sentence.split(" ")
+  puts sentence_array
 end
